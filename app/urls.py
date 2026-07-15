945db2239ecc12270be48a441da61a30cd23b8e7
DostoevskiyRiuui <Hobbycool9@gmail.com>
2026-07-15 13:38:25 +0500

    CRUD

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from .views import PostView
-urlpatterns = [
-    path("", PostView.as_view({'post':'create'})),
-]
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('posts', PostView)
+
+urlpatterns = router.urls
